d4e3355cc0f073d3dabc53447956b15f164f7a79
souravvmishra <sourav@anoseo.com>
2024-12-19 12:30:28 +0530

    added buy me a coffee

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,12 +1,10 @@
 import Hero from '@/components/main/Hero'
 import About from '@/components/main/About'
-import { FloatingEmojis } from './Page404'
 const Homepage = () => {
   return (
     <div>
       <Hero />
       <About />
-      <FloatingEmojis />
     </div>
   )
 }
